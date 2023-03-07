487e34901c542cafd91631c89a017d98134ca238
Levy <kipkoechlevy096@gmail.com>
2023-03-07 17:55:36 +0300

    uncomented code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < Sinatra::Base
 
 
-    # configure do
-    #   enable :cross_origin
-    # end
+    configure do
+      enable :cross_origin
+    end
   
-    # before do
-    #   response.headers['Access-Control-Allow-Origin'] = '*'
-    # end
+    before do
+      response.headers['Access-Control-Allow-Origin'] = '*'
+    end
   
-    # options "*" do
-    #   response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
-    #   response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
-    #   response.headers["Access-Control-Allow-Origin"] = "*"
-    #   200
-    # end
+    options "*" do
+      response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+      response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+      response.headers["Access-Control-Allow-Origin"] = "*"
+      200
+    end
   
     def json_response(code: 200, data: nil)
       status = [200, 201].include?(code) ? "SUCCESS" : "FAILED"
