99eda7227eab4968b9c4c76750481748fd2e3b47
Levy <kipkoechlevy096@gmail.com>
2023-03-08 20:11:08 +0300

    modified cors code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,19 @@
 class ApplicationController < Sinatra::Base
 
-configure do
-  enable :cross_origin
-end
-before do
-  headers['Access-Control-Allow-Origin'] = 'https://tourmaline-taffy-23a213.netlify.app'
-  headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-  headers['Access-Control-Allow-Headers'] = 'accept, content-type'
-end
+  configure do
+    enable :cross_origin
+  end
 
-options "*" do
-  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
-  response.headers["Access-Control-Allow-Origin"] = "https://tourmaline-taffy-23a213.netlify.app"
-  200
-end
+  before do
+    response.headers['Access-Control-Allow-Origin'] = '*'
+  end
 
+  options "*" do
+    response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    200
+  end
   
   
     def json_response(code: 200, data: nil)
